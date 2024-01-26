168a81768fcb55e46e2cdbc34ece212f6840a6e4
evilfoxes <evilfoxes@mail.ru>
2024-01-26 15:10:55 +1100

    Save_HW1

---
+++ src/Lesson_1/Home_Work/Task_1.java
@@ -0,0 +1,32 @@
+class Answer {
+    public int countNTriangle(int n){
+        // Введите свое решение ниже
+        double res = 0.5;
+        if (n >= 1) {
+            res = res * n * (n + 1);
+            return (int) res;
+        } else {
+            return -1;
+        }
+    }
+}
+
+// Не удаляйте этот класс - он нужен для вывода результатов на экран и проверки
+public class Printer{
+    public static void main(String[] args) {
+        int n = 0;
+
+        if (args.length == 0) {
+            // При отправке кода на Выполнение, вы можете варьировать эти параметры
+            n = 4;
+        }
+        else{
+            n = Integer.parseInt(args[0]);
+        }
+
+        // Вывод результата на экран
+        Answer ans = new Answer();
+        int itresume_res = ans.countNTriangle(n);
+        System.out.println(itresume_res);
+    }
+}
