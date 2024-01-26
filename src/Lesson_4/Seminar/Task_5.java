cb0d1a09ee3ab84d870853a894d053a604d20f82
evilfoxes <evilfoxes@mail.ru>
2024-01-26 18:04:09 +1100

    Save_Seminar_4_4

---
+++ src/Lesson_4/Seminar/Task_5.java
@@ -0,0 +1,13 @@
+public class Task_5 {
+    public static void main(String[] args) {
+        MyStack stack = new MyStack();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+
+        MyStack stack1 = new MyStack();
+        System.out.println(stack);
+        System.out.println(stack1);
+    }
+}
